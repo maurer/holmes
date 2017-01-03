d0590aa88936a0863fcbcb9aa99c5929effacfb8
Matthew Maurer <matthew.r.maurer@gmail.com>
2017-01-02 19:04:07 -0500

    Style flake

--- tests/fact_basic.rs
+++ tests/fact_basic.rs
@@ -5,7 +5,7 @@ pub fn new_fact_basic() {
     single(&|holmes: &mut Holmes| {
         holmes_exec!(holmes, {
             predicate!(test_pred(string, bytes, uint64));
-            fact!(test_pred("foo", vec![3u8,4u8,5u8], 7))
+            fact!(test_pred("foo", vec![3u8, 4u8, 5u8], 7))
         })
     })
 }
@@ -15,7 +15,7 @@ pub fn new_fact_type_err() {
     single(&|holmes: &mut Holmes| {
         holmes_exec!(holmes, {
             predicate!(test_pred(string, bytes, uint64));
-            should_fail(fact!(test_pred(7, vec![3u8,4u8,5u8], 7)))
+            should_fail(fact!(test_pred(7, vec![3u8, 4u8, 5u8], 7)))
         })
     })
 }
@@ -25,7 +25,7 @@ pub fn new_fact_echo() {
     single(&|holmes: &mut Holmes| {
         try!(holmes_exec!(holmes, {
             predicate!(test_pred(string, bytes, uint64));
-            fact!(test_pred("foo", vec![3u8,3u8], 7))
+            fact!(test_pred("foo", vec![3u8, 3u8], 7))
     }));
         assert_eq!(query!(holmes,
                        test_pred(("foo"), [_], x)).unwrap(),
--- tests/reboot.rs
+++ tests/reboot.rs
@@ -9,13 +9,13 @@ fn simple() {
 fn pred_compat() {
     multi(&[&|holmes: &mut Holmes| {
                 holmes_exec!(holmes, {
-        predicate!(test_pred(bytes, uint64))
-    })
+                    predicate!(test_pred(bytes, uint64))
+                })
             },
             &|holmes: &mut Holmes| {
                 holmes_exec!(holmes, {
-        predicate!(test_pred(bytes, uint64))
-    })
+                    predicate!(test_pred(bytes, uint64))
+                })
             }])
 }
 
@@ -23,13 +23,13 @@ fn pred_compat() {
 fn pred_incompat() {
     multi(&[&|holmes: &mut Holmes| {
                 holmes_exec!(holmes, {
-        predicate!(test_pred(bytes, uint64))
-    })
+                    predicate!(test_pred(bytes, uint64))
+                })
             },
             &|holmes: &mut Holmes| {
                 holmes_exec!(holmes, {
-        should_fail(predicate!(test_pred(bytes, uint64, uint64)))
-    })
+                    should_fail(predicate!(test_pred(bytes, uint64, uint64)))
+                })
             }])
 }
 
@@ -37,9 +37,9 @@ fn pred_incompat() {
 fn fact_preserve() {
     multi(&[&|holmes: &mut Holmes| {
                 holmes_exec!(holmes, {
-        predicate!(test_pred(string, uint64));
-        fact!(test_pred("foo", 7))
-    })
+                    predicate!(test_pred(string, uint64));
+                    fact!(test_pred("foo", 7))
+                })
             },
             &|holmes: &mut Holmes| {
                 assert_eq!(query!(holmes, test_pred(("foo"), x)).unwrap(),
