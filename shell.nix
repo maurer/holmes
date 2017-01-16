8cbf5e49c1fcef44da30d2c781d497c5d806bcf8
Matthew Maurer <matthew.r.maurer@gmail.com>
2017-01-16 11:11:07 -0500

    Add default vim config

--- shell.nix
+++ shell.nix
@@ -1,10 +1,36 @@
 { nixpkgs ? import <nixpkgs> {}}:
 nixpkgs.callPackage (
-{ stdenv, rust, openssl, postgresql }:
+{ stdenv, rust, openssl, postgresql, vim_configurable }:
+
+# Vim with rust + git support
+let vim = vim_configurable.customize {
+  name = "vim-holmes";
+  vimrcConfig.customRC = ''
+    set backspace=indent,eol,start
+
+    let g:rustfmt_autosave = 1
+    autocmd FileType rust compiler cargo
+    let g:racer_cmd = "${nixpkgs.rustracer}/bin/racer"
+    let $RUST_SRC_PATH="${nixpkgs.rustc.src}/src"
+    let g:racer_experimental_completer = 1
+    au FileType rust nmap gd <Plug>(rust-def)
+    au FileType rust nmap gs <Plug>(rust-def-split)
+    au FileType rust nmap gx <Plug>(rust-def-vertical)
+    au FileType rust nmap <leader>gd <Plug>(rust-doc)
+  '';
+  vimrcConfig.vam.pluginDictionaries = [{ names = [
+    "fugitive"
+    "vim-racer"
+    "rust-vim"
+    "syntastic"
+  ];}];
+}; in
+
 with rust;
 stdenv.mkDerivation rec {
   name = "holmes";
   buildInputs = [ cargo rustc
-                  openssl postgresql ];
+                  openssl postgresql
+                  vim ];
 }
 ) {}
