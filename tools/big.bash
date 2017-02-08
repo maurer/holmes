e54307312bb08abb399d79edd5c1ff04ea58cbd8
Matthew Maurer <matthew.r.maurer@gmail.com>
2017-02-08 14:10:39 -0500

    Remove commit sync for one-off databases

--- tools/big.bash
+++ tools/big.bash
@@ -4,6 +4,7 @@ pg_ctl initdb -s -o -Atrust
 echo "unix_socket_directories = '$PGDATA'" >> $PGDATA/postgresql.conf
 echo "listen_addresses = ''" >> $PGDATA/postgresql.conf
 echo "work_mem = 1GB" >> $PGDATA/postgresql.conf
+echo "synchronous_commit = off" >> $PGDATA/postgresql.conf
 pg_ctl -w start -s -l/dev/null
 createuser -h $PGDATA -s $1
 echo $PGDATA
--- tools/pg.bash
+++ tools/pg.bash
@@ -6,6 +6,7 @@ echo "listen_addresses = ''" >> $PGDATA/postgresql.conf
 #echo "logging_collector = true" >> $PGDATA/postgresql.conf
 #echo "log_directory = '$PGDATA'" >> $PGDATA/postgresql.conf
 #echo "log_statement = 'all'" >> $PGDATA/postgresql.conf
+echo "synchronous_commit = off" >> $PGDATA/postgresql.conf
 pg_ctl -w start -s -l/dev/null
 createuser -h $PGDATA -s $1
 echo $PGDATA
